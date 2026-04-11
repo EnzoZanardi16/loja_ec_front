5b8cb23d63bbeacd0a22451807e1402699bb9a75
EnzoZanardi16 <enzozanardi48@gmail.com>
2026-04-11 01:43:22 -0300

    feat: cria as telas

---
+++ src/Screens/Carrinho.jsx
@@ -0,0 +1,11 @@
+export default function Carrinho() {
+  return (
+    <div>
+      <h1>Carrinho</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  );
+}
---
+++ src/Screens/Login.jsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+  );
+}
