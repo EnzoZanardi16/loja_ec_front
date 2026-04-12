85419808da017f4639021c1f270d5da7193ee5a3
EnzoZanardi16 <enzozanardi48@gmail.com>
2026-04-12 18:47:44 -0300

    feat: integra o login

---
+++ src/Context/AuthContext.jsx
@@ -0,0 +1,18 @@
+import { createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) setIsAuthenticated(true);
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
--- src/Screens/Login.jsx
+++ src/Screens/Login.jsx
@@ -1,11 +1,101 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../Context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const { setIsAuthenticated } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault(); // evita recarregar a página
+
+    try {
+      console.log({ email, password });
+     const response = await fetch("http://localhost:8000/api/auth/login", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  credentials: "include",
+  body: JSON.stringify({
+    email,
+    password,
+  }),
+});
+
+      const data = await response.json();
+
+      if (response.ok) {
+        // salva token (ajuste conforme seu backend)
+        console.log(data);
+        //localStorage.setItem("token", data.token);
+        alert("sucess");
+
+        setIsAuthenticated(true);
+        navigate("/");
+      } else {
+        alert(data.message || "Erro ao fazer login");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Erro ao conectar com o servidor");
+    }
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-    </div>
+    <section className="d-flex justify-content-center align-items-center">
+      <div className="login-container p-4 rounded-4 shadow">
+        
+        <h1 className="text-center">Login</h1>
+
+        <form onSubmit={handleLogin}>
+          
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="check"
+            />
+            <label className="form-check-label" htmlFor="check">
+              Check me out
+            </label>
+          </div>
+
+          <button type="submit" className="btn btn-primary w-100">
+            Entrar
+          </button>
+
+        </form>
+      </div>
+    </section>
   );
 }
