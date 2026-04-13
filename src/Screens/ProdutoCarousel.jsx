ea11a2e7d5c86aaf94112d77c47ba21368d3b6b0
EnzoZanardi16 <enzozanardi48@gmail.com>
2026-04-13 16:29:16 -0300

    fix: exibe os produtos do banco

--- src/Screens/ProdutoCarousel.jsx
+++ src/Screens/ProdutoCarousel.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 // import { data } from "react-router-dom";
+import "../Components/ProdutoCarousel.css"
+
 
 
 export default function ProdutoCarousel(){
@@ -11,7 +13,9 @@ export default function ProdutoCarousel(){
         fetch("http://localhost:8000/api/produtos")
 
             .then(res => res.json())
-            .then(data => setProducts(data))
+            .then(data => {
+                setProducts(data);
+            })
             .catch(err => console.error(err))
 },[]);
 const scrollLeft = () => {
@@ -24,24 +28,29 @@ const scrollRight = () => {
 
 return (
     <div className="position-relative">
-
-    <button className="btn btn-dark position-absolute start-0 top-50 z-3" onClick={scrollLeft}>
-        ◀ 
-    </button>
+<button 
+  className="position-absolute top-50 start-0 translate-middle-y border-0 bg-transparent px-2 z-3"
+  onClick={scrollLeft}
+>
+  <span className="carousel-control-prev-icon" />
+</button>
     <div ref={scrollRef} className="d-flex overflow-auto gap-3 p-3" style={{ scrollBehavior: "smooth"}}>
-        {products.map(product => (
-            <div key={product.id} className="card" style={{minWidth: "200px"}}>
-                <img src={product.image} className="card-img-top" alt="" />
+        {products.map(produto => (
+            <div key={produto.id} className="card" style={{minWidth: "200px"}}>
+                <img src={produto.imagem} className="card-img-top" alt="" />
                 <div className="card-body">
-                    <h6>{product.name}</h6>
-                    <p>R$ {product.price}</p>
+                    <h6>{produto.nome}</h6>
+                    <p>R$ {produto.preco}</p>
                 </div>
             </div>
         ))}
     </div>
-          <button className="btn btn-dark position-absolute end-0 top-50 z-3" onClick={scrollRight}>
-        ▶
-      </button>
+<button 
+  className="position-absolute top-50 end-0 translate-middle-y border-0 bg-transparent px-2 z-3"
+  onClick={scrollRight}
+>
+  <span className="carousel-control-next-icon" />
+</button>
 
     </div>
 );
