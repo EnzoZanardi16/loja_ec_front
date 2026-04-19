82d57ade4c7c69209299fda13af6792c7ce5cadd
EnzoZanardi16 <enzozanardi48@gmail.com>
2026-04-19 17:30:04 -0300

    feat: adiciona a bara de categorias ao home

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,41 +1,47 @@
 import banner from "../Assets/banner-teste.webp";
 import "../Components/Home.css"
 import ProdutoCarousel from "./ProdutoCarousel";
+import CategoriaBar from "./CategoriaBar";
 export default function Home() {
   return (
     <main>
-            <div id="carouselExampleInterval" className="carousel slide carosel-custom" data-bs-ride="carousel">
+      <div id="carouselExampleInterval" className="carousel slide carosel-custom" data-bs-ride="carousel">
 
-                              <div className="carousel-indicators">
+        <div className="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
 
-            <div className="carousel-inner">
-              <div className="carousel-item active" data-bs-interval="10000">
-                <img src={banner} className="d-block w-100" alt="banner azul de promoção"/>
-              </div>
-              <div className="carousel-item" data-bs-interval="2000">
-                <img src={banner} className="d-block w-100" alt="..."/>
-              </div>
-              <div className="carousel-item">
-                <img src={banner} className="d-block w-100" alt="..."/>
-              </div>
-            </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-              <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-              <span className="carousel-control-next-icon" aria-hidden="true"></span>
-              <span className="visually-hidden">Next</span>
-            </button>
+        <div className="carousel-inner">
+          <div className="carousel-item active" data-bs-interval="10000">
+            <img src={banner} className="d-block w-100" alt="banner azul de promoção" />
           </div>
+          <div className="carousel-item" data-bs-interval="2000">
+            <img src={banner} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={banner} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
 
-          <div className="section-products">
-            <ProdutoCarousel />
-          </div>
+      <div className="section-categorias">
+        <CategoriaBar />
+      </div>
+
+
+      <div className="section-products">
+        <ProdutoCarousel />
+      </div>
 
     </main>
 
